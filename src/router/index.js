2f85b93f33cb15ef4d62d87bfcd36e3f9f5054c2
zgqallan <zgqallan@gmail.com>
2021-09-22 13:45:19 +0800

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex,
+      meta: {
+        requireAuth: true
+      }
+    }
+  ]
+})
